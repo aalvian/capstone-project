c2b07d7e94927e54f0c505421cbac51c9f28d305
M. Alvian Ari Nugroho <aalvian052@gmail.com>
2025-06-12 02:14:14 +0700

    Update Backend

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,7 +6,7 @@ import App from './pages/app';
 
 document.addEventListener('DOMContentLoaded', async () => {
   // const response = await fetch('http://localhost:3000/api/check-db');
-  console.log('Database', response);
+  // console.log('Database', response);
 
   const app = new App({
     content: document.querySelector('#main-content'),
