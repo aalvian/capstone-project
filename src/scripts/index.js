f0f615fc1111510f6df4f63038c086aa2f98a506
M. Alvian Ari Nugroho <aalvian052@gmail.com>
2025-06-12 02:04:45 +0700

    Update Backend

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,7 +5,7 @@ import '../styles/pomodoro.css';
 import App from './pages/app';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const response = await fetch('http://localhost:3000/api/check-db');
+  // const response = await fetch('http://localhost:3000/api/check-db');
   console.log('Database', response);
 
   const app = new App({
